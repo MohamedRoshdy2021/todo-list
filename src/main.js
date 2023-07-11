405cbbe6b1e7a16f7f30f8f449f038bf4ea88e99
Mohammed roshdy <xmedox20111@gmail.com>
2023-07-11 19:55:06 +0300

    Fixing linters

--- src/main.js
+++ src/main.js
@@ -1,26 +1,24 @@
 import './style.css';
-const cont = document.getElementById("todo-list")
-const tasklist = document.getElementById("list-item")
-let tasks = [
-    {   
-    description: "driving",
-    completed :false,
-    index:0
-    },
-     {   
-    description: "styding",
-    completed :true,
-    index:1
-    },
-      {   
-    description: "swimming",
-    completed :false,
-    index:2
-    },
+
+const tasklist = document.getElementById('list-item');
+const tasks = [
+  {
+    description: 'driving',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'styding',
+    completed: true,
+    index: 1,
+  },
+  {
+    description: 'swimming',
+    completed: false,
+    index: 2,
+  },
 ];
 
 tasks.forEach((element) => {
-  tasklist.innerHTML += `<div class="li-div"><div id="li-id"><input type="checkbox">${element.description}</div><i class="fa-solid fa-ellipsis-vertical"></i></div>`
-})
-
-
+  tasklist.innerHTML += `<div class="li-div"><div id="li-id"><input type="checkbox">${element.description}</div><i class="fa-solid fa-ellipsis-vertical"></i></div>`;
+});
