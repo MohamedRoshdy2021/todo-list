5cb661d48fcbee53462cd8427f86c012cbeab632
Mohammed roshdy <xmedox20111@gmail.com>
2023-07-11 14:43:59 +0300

    Added linters to the project

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
-import './style.css'
+import './style.css';
+function mo() {
+    
 
+const m = document.createElement('h2');
+m.textContent = 'jhsdfjkfhjkdsfndsfsdfdsfdsfsdfdsfdsf <br> ygusadkjsahdsajhdlksadsadsad';
+    document.querySelector('.div').appendChild(m);
+}
 
-let m = document.createElement("h2")
-m.textContent = `jhsdfjkfhjkdsfndsfsdfdsfdsfsdfdsfdsf <br> ygusadkjsahdsajhdlksadsadsad`
-document.querySelector(".div").appendChild(m)
+mo()
--- webpack.config.js
+++ webpack.config.js
@@ -2,34 +2,34 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/main.js',
-    mode: 'development',
-    plugins: [
+  entry: './src/main.js',
+  mode: 'development',
+  plugins: [
     new HtmlWebpackPlugin({
-        title: 'Development',
-        template: './src/index.html',
+      title: 'Development',
+      template: './src/index.html',
     }),
-    ],
-    devtool: 'inline-source-map',
-    devServer: {
+  ],
+  devtool: 'inline-source-map',
+  devServer: {
     static: './dist',
-    },
-    
+  },
+
   output: {
     filename: 'main.js',
-      path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    },
-   module: {
-       rules: [
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
-           },
-         {
+      },
+      {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
     ],
-    },
+  },
 };
