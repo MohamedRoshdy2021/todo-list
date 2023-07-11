bf548fcecf495c196a7d69facb6a98027a5fa40a
Mohammed roshdy <xmedox20111@gmail.com>
2023-07-11 14:34:05 +0300

    Added the init files for the webpack

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import './style.css'
+
+
+let m = document.createElement("h2")
+m.textContent = `jhsdfjkfhjkdsfndsfsdfdsfdsfsdfdsfdsf <br> ygusadkjsahdsajhdlksadsadsad`
+document.querySelector(".div").appendChild(m)
